0e4f4ef190cdf97102ec35cbe2fe425a650536fd
yechens <708876062@qq.com>
2020-05-19 00:31:30 +0800

    xyc leetcode 0518

---
+++ coding/104_二叉树的最大深度.py
@@ -0,0 +1,24 @@
+"""
+题目：
+给定一个二叉树，找出其最大深度。
+二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+说明: 叶子节点是指没有子节点的节点。
+
+示例：
+给定二叉树 [3,9,20,null,null,15,7]，
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回它的最大深度 3 
+
+解题思路：
+递归遍历左右子树，返回左子树、右子树中的最大高度 + 1 即为当前root根结点的深度
+"""
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
